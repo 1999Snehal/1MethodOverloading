3d558da4eada4ec2b094102ccc611d7d2c6b7008
1999Snehal <snehallagad84@gmail.com>
2024-11-05 17:13:38 +0530

    commited

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+class Add
+{
+    static int add(int a,int b)
+    {
+        return a+b;
+    }
+    static  double add(double a, double b)
+    {
+        return  a+b;
+    }
+}
+class  MethodOverloading
+{
+
+    public  static void main(String[] args)
+    {
+
+
+        System.out.println(Add.add(45,75));
+        System.out.println(Add.add(69.5,59.9));
+
+
+
     }
 }
